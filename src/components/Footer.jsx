aa22bd6f483bd2c13e21f38ee5ac845859573e26
Abhishek Choudhary <abhishekchoudharyorg@gmail.com>
2025-10-01 14:17:51 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="logo font-bold text-2xl">
         <span className="text-green-500 ">&lt;</span>
         Pass
-        <span className="text-green-500 ">OP/ &gt;</span>
+        <span className="text-green-500 ">OP/&gt;</span>
       </div>
       <div className="flex justify-center items-center">
         Made with <img className="w-6 mx-2" src="icons/heart.png" alt="" /> by Abhishek
